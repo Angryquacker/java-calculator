6608db7166a0efebdb6c9a8aabc05f6b04515c3f
Travis <tjxhahn@gmail.com>
2018-02-05 13:41:35 -0500

    Create socialNumGen.java

---
+++ socialNumGen.java
@@ -0,0 +1,20 @@
+import java.lang.Math;
+
+public class SocialNum {
+    public void SocialNum() {
+        
+    }
+    
+    public static void main(String[] args) {
+        long num1 = Math.round(Math.floor(Math.random() * 10)); 
+        long num2 = Math.round(Math.floor(Math.random() * 10));  
+        long num3 = Math.round(Math.floor(Math.random() * 10)); 
+        long num4 = Math.round(Math.floor(Math.random() * 10));  
+        long num5 = Math.round(Math.floor(Math.random() * 10)); 
+        long num6 = Math.round(Math.floor(Math.random() * 10));  
+        long num7 = Math.round(Math.floor(Math.random() * 10));  
+        long num8 = Math.round(Math.floor(Math.random() * 10)); 
+        long num9 = Math.round(Math.floor(Math.random() * 10)); 
+        System.out.println(num1 + "" + num2 + "" + num3 + "-" + num4 + "" + num5 + "-" + num6 + "" + num7 + "" + num8 + "" + num9);
+    }
+}
